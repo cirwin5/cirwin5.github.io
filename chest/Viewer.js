d9b31b02e47d9623ee8edfbe22a12f5a50fb0d48
cirwin5 <cirwin000@gmail.com>
2015-10-20 21:37:03 -0500

    Update Viewer.js

--- chest/Viewer.js
+++ chest/Viewer.js
@@ -74,7 +74,7 @@ function flatten(a) {
 	return a.reduce(function (b, v) { b.push.apply(b, v); return b; }, [])
 }
 
-function chestInit() {
+function init() {
 	
 	//Initialize the WebGL context.
 	
